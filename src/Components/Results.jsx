29a469cd6c3c8f654ba8294aac259952f373e6ec
AnonomousSharma <129974758+AnonomousSharma@users.noreply.github.com>
2025-01-03 14:43:59 +0530

    Update Results.jsx

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
-const Results = ({ results, onOpenDisclaimer }) => {
+const Results = ({ results, onOpenDisclaimer, language }) => {
   if (!results) {
     return (
       <div className="results">
-        <h2>Results</h2>
-        <p>No input data yet. Please enter values to see the results.</p>
+        <h2>{language === "en" ? "Results" : "परिणाम"}</h2>
+        <p>{language === "en" ? "No input data yet. Please enter values to see the results." : "कोई डेटा नहीं है। कृपया परिणाम देखने के लिए मान दर्ज करें।"}</p>
       </div>
     );
   }
 
   return (
     <div className="results">
-      <h2>Solar Panel Installation Results</h2>
+      <h2>{language === "en" ? "Solar Panel Installation Results" : "सोलर पैनल इंस्टॉलेशन परिणाम"}</h2>
       <ul>
-        <li>Solar Capacity: <strong>{results.solarCapacity} kW</strong></li>
-        <li>Required Area for Solar: <strong>{results.area} sq. ft.</strong> </li>
-        <li>Monthly Unit Production:  <strong>{results.monthlyProduction} units</strong> </li>
-        <li>Off-Grid Installation Cost: <strong>₹{results.offGridCost}</strong> </li>
-        <li>On-Grid Installation Cost: <strong>₹{results.onGridCost}</strong></li>
+        <li>{language === "en" ? "Solar Capacity: " : "सोलर क्षमता: "}<strong>{results.solarCapacity} kW</strong></li>
+        <li>{language === "en" ? "Required Area for Solar: " : "सोलर के लिए आवश्यक क्षेत्र: "}<strong>{results.area} sq. ft.</strong></li>
+        <li>{language === "en" ? "Monthly Unit Production: " : "मासिक उत्पादन: "}<strong>{results.monthlyProduction} units</strong></li>
+        <li>{language === "en" ? "Off-Grid Installation Cost: " : "ऑफ-ग्रिड इंस्टॉलेशन लागत: "}<strong>₹{results.offGridCost}</strong></li>
+        <li>{language === "en" ? "On-Grid Installation Cost: " : "ऑन-ग्रिड इंस्टॉलेशन लागत: "}<strong>₹{results.onGridCost}</strong></li>
       </ul>
-      <button onClick={onOpenDisclaimer}>View Disclaimer</button>
+      <button onClick={onOpenDisclaimer}>{language === "en" ? "View Disclaimer" : "डिस्क्लेमर देखें"}</button>
     </div>
   );
 };
